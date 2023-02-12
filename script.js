0ad2d1ab930b6d40bf45928a9f68eed9a5fcad7e
Dima <mitya.zavalnui@gmail.com>
2023-02-12 23:27:55 +0200

    first js program

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log(223);
+'use strict';
+const numberOfFilms = +prompt('Скільки фільмів ви вже подивились?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один із останніх переглянутих фільмів', ''),
+      b = prompt('Ваша оцінка даного фільму', ''),
+      c = prompt('Один із останніх переглянутих фільмів', ''),
+      d = prompt('Ваша оцінка даного фільму', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
